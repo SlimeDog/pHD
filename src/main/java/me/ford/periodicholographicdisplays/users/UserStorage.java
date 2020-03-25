8584d92c5373174408b24bb3b9596f6fdddf045e
Mart Ratas <mart.ratas@gmail.com>
2020-03-25 08:34:47 +0200

    Added the 'populate' method to the UserStorage interface. It defaults to populating the cache with all offline player entries.

--- src/main/java/me/ford/periodicholographicdisplays/users/UserStorage.java
+++ src/main/java/me/ford/periodicholographicdisplays/users/UserStorage.java
@@ -1,5 +1,8 @@
 package me.ford.periodicholographicdisplays.users;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
 /**
  * UserManager
  */
@@ -8,5 +11,12 @@ public interface UserStorage {
     public UserCache getCache();
 
     public void save(boolean inSync);
+
+    public default void populate() {
+        for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
+            getCache().set(player.getUniqueId(), player.getName());
+        }
+        save(false);
+    }
     
 }
