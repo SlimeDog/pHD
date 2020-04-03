bd090d11dcfd19baac189b620464eb04fb7e7aca
Mart Ratas <mart.ratas@gmail.com>
2020-04-03 06:24:48 +0300

    Added a MockHologram that provides location

---
+++ src/test/java/me/ford/periodicholographicdisplays/mock/MockHologram.java
@@ -0,0 +1,150 @@
+package me.ford.periodicholographicdisplays.mock;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.gmail.filoghost.holographicdisplays.api.VisibilityManager;
+import com.gmail.filoghost.holographicdisplays.api.line.HologramLine;
+import com.gmail.filoghost.holographicdisplays.api.line.ItemLine;
+import com.gmail.filoghost.holographicdisplays.api.line.TextLine;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.inventory.ItemStack;
+
+public class MockHologram implements Hologram {
+
+    @Override
+    public ItemLine appendItemLine(ItemStack arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public TextLine appendTextLine(String arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void clearLines() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void delete() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public long getCreationTimestamp() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getHeight() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public HologramLine getLine(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Location getLocation() {
+        Random rnd = ThreadLocalRandom.current();
+        return new Location(new MockWorld("mockery"), rnd.nextDouble(), rnd.nextDouble(), rnd.nextDouble());
+    }
+
+    @Override
+    public VisibilityManager getVisibilityManager() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public World getWorld() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double getX() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getY() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getZ() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public ItemLine insertItemLine(int arg0, ItemStack arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public TextLine insertTextLine(int arg0, String arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isAllowPlaceholders() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isDeleted() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void removeLine(int arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setAllowPlaceholders(boolean arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void teleport(Location arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void teleport(World arg0, double arg1, double arg2, double arg3) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
