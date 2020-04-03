d00ffe5f27b7c850a99be3205946dcf465376a0a
Mart Ratas <mart.ratas@gmail.com>
2020-04-03 22:12:33 +0300

    Added initial tests for manage command (i.e generic illegal usage)

---
+++ src/test/java/me/ford/periodicholographicdisplays/commands/CommandManageTests.java
@@ -0,0 +1,42 @@
+package me.ford.periodicholographicdisplays.commands;
+
+import org.junit.Test;
+
+import me.ford.periodicholographicdisplays.commands.subcommands.ManageSub;
+import me.ford.periodicholographicdisplays.holograms.PeriodicType;
+import me.ford.periodicholographicdisplays.mock.MockNamedHologram;
+
+public class CommandManageTests extends BaseCommandTests {
+    // USAGE = "/phd manage <hologram> <type> times <integer> time <hh:mm> [<options...>]";
+
+    @Test
+    public void testManageIllegal() {
+        // phd manage NonExisting
+        String name = "hdHologram";
+        String expectedMessage = phd.getMessages().getHDHologramNotFoundMessage(name);
+        testCommand(sender, null, "phd", new String[] {"manage", name}, expectedMessage);
+        // phd manage existing
+        // add mock hologram
+        phd.putHDHologram(name, new MockNamedHologram(name));
+        String usageMessage = new ManageSub(phd).getUsage(sender);
+        expectedMessage = usageMessage;
+        testCommand(sender, null, "phd", new String[] {"manage", name}, expectedMessage);
+
+        // phd manage existing <incorrectType>
+        String fakeType = "FAKE";
+        expectedMessage = phd.getMessages().getTypeNotRecognizedMessage(fakeType);
+        testCommand(sender, null, "phd", new String[] {"manage", name, fakeType}, expectedMessage);
+
+
+        // phd manage existing <type> # not ALWAYS
+        PeriodicType type = PeriodicType.NTIMES;
+        expectedMessage = usageMessage;
+        testCommand(sender, null, "phd", new String[] {"manage", name, type.name()}, expectedMessage);
+
+        // phd manage existing <type> <option> <value> <option_with_no_value> # not ALWAYS
+        expectedMessage = phd.getMessages().getNeedPairedOptionsMessage();
+        testCommand(sender, null, "phd", new String[] {"manage", name, type.name(), "opt", "val", "opt2"}, expectedMessage);
+
+    }
+
+}
