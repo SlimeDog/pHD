eb41990b0d403b5fa9b3c7d41daebad01b3908f0
Mart Ratas <mart.ratas@gmail.com>
2020-04-03 23:09:13 +0300

    Made the set-manage common method manage-friendly

--- src/test/java/me/ford/periodicholographicdisplays/commands/BaseCommandTests.java
+++ src/test/java/me/ford/periodicholographicdisplays/commands/BaseCommandTests.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 
+import me.ford.periodicholographicdisplays.commands.subcommands.ManageSub;
 import me.ford.periodicholographicdisplays.commands.subcommands.SetSub;
 import me.ford.periodicholographicdisplays.holograms.FlashingHologram;
 import me.ford.periodicholographicdisplays.holograms.PeriodicType;
@@ -29,23 +30,33 @@ public abstract class BaseCommandTests {
         phd.clear();
     }
 
-    protected void testSetManageCommonIllegals(FlashingHologram hologram, String holoName, PeriodicType wrongType, boolean isSet) {
+    protected void testSetManageCommonIllegals(FlashingHologram hologram, String holoName, PeriodicType testType, boolean isSet) {
+        String commandName = isSet ? "set" : "manage";
         // /phd set <name>
-        String expected = new SetSub(phd.getHolograms(), null, phd.getSettings(), phd.getMessages()).getUsage(sender);
-        testCommand(sender, null, "phd", new String[] { "set", holoName}, expected);
+        String usageMessage;
+        if (isSet) {
+            usageMessage = new SetSub(phd.getHolograms(), phd.getLuckPermsHook(), phd.getSettings(), phd.getMessages()).getUsage(sender);
+        } else {
+            usageMessage = new ManageSub(phd).getUsage(sender);
+        }
+        String expected = usageMessage;
+        testCommand(sender, null, "phd", new String[] {commandName, holoName}, expected);
         // /phd set <name> 1
         expected = phd.getMessages().getTypeNotRecognizedMessage("1");
-        testCommand(sender, null, "phd", new String[] { "set", holoName, "1"}, expected);
+        testCommand(sender, null, "phd", new String[] {commandName, holoName, "1"}, expected);
         // /phd set <name> <wrongtype>
-        expected = new SetSub(phd.getHolograms(), phd.getLuckPermsHook(), phd.getSettings(),
-                    phd.getMessages()).getUsage(sender);
-        testCommand(sender, null, "phd", new String[] { "set", holoName, wrongType.name()}, expected);
+        expected = usageMessage;
+        testCommand(sender, null, "phd", new String[] {commandName, holoName, testType.name()}, expected);
         // /phd set <name> <type> <option> <value>
-        expected = phd.getMessages().getHologramNotTrackedMessage(holoName, wrongType);
-        testCommand(sender, null, "phd", new String[] { "set", holoName, wrongType.name(), "option", "value"}, expected);
+        if (isSet) {
+            expected = phd.getMessages().getHologramNotTrackedMessage(holoName, testType);
+        } else {
+            expected = phd.getMessages().getHologramAlreadyManagedMessage(holoName, testType);
+        }
+        testCommand(sender, null, "phd", new String[] {commandName, holoName, testType.name(), "option", "value"}, expected);
         // /phd set <name> <type> <option> <value> <option2> # no value
         expected = phd.getMessages().getNeedPairedOptionsMessage();
-        testCommand(sender, null, "phd", new String[] {"set", holoName, wrongType.name(), "option", "value", "option2"}, expected);
+        testCommand(sender, null, "phd", new String[] {commandName, holoName, testType.name(), "option", "value", "option2"}, expected);
     }
 
     protected void testCommand(MockOPCommandSender sender, Command command, String label, String args[], String expectedMessage) {
