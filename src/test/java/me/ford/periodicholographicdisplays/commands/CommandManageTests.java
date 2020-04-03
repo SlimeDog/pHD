a13382c217a4799856c478d14c465aedd9e66e02
Mart Ratas <mart.ratas@gmail.com>
2020-04-03 23:09:36 +0300

    Added test for managing of illegal NTIMES holoograms

--- src/test/java/me/ford/periodicholographicdisplays/commands/CommandManageTests.java
+++ src/test/java/me/ford/periodicholographicdisplays/commands/CommandManageTests.java
@@ -43,7 +43,16 @@ public class CommandManageTests extends BaseCommandTests {
         phd.getHolograms().addHologram(hologram);
         expectedMessage = phd.getMessages().getHologramAlreadyManagedMessage(name, type);
         testCommand(sender, null, "phd", new String[] {"manage", name, type.name(), "opt", "val"}, expectedMessage);
+    }
 
+    @Test
+    public void testManageIllegalGeneral() {
+        String name = "hdHol0gram";
+        MockNamedHologram mnh = new MockNamedHologram(name);
+        phd.putHDHologram(name, mnh);
+        NTimesHologram hologram = new NTimesHologram(phd, mnh, name, 2.2, 6, 4, true, "super.perms", 4, 2);
+        phd.getHolograms().addHologram(hologram);
+        this.testSetManageCommonIllegals(hologram, name, PeriodicType.NTIMES, false);
     }
 
 }
