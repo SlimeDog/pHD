46442bdd3bf4787d912410c9249005f53429cba6
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-01-08 22:06:34 +0000

    Moved NMS stuff slightly in line tracker wrapper

--- src/main/java/me/ford/periodicholographicdisplays/holograms/visbility/LineTrackerWrapper.java
+++ src/main/java/me/ford/periodicholographicdisplays/holograms/visbility/LineTrackerWrapper.java
@@ -13,67 +13,16 @@ import me.filoghost.holographicdisplays.plugin.hologram.tracking.LineTracker;
 import me.filoghost.holographicdisplays.plugin.hologram.tracking.MutableViewers;
 
 public class LineTrackerWrapper {
-    private static final Field VIEWERS_FIELD;
-    private static final Class<?> VIEWER_CLASS;
-    private static final Class<?> VIEWERS_CLASS;
-    private static final Method LINETRACKER_SEND_SPAWN_METHOD;
-    private static final Method LINETRACKER_SEND_DESTROY_METHOD;
-    private static final Method VIEWERS_ADD_METHOD;
-    static {
-        Field f;
-        try {
-            f = LineTracker.class.getDeclaredField("viewers");
-        } catch (NoSuchFieldException | SecurityException e) {
-            throw new RuntimeException(e);
-        }
-        VIEWERS_FIELD = f;
-        VIEWERS_FIELD.setAccessible(true);
-        Class<?> c;
-        try {
-            c = Class.forName("me.filoghost.holographicdisplays.plugin.hologram.tracking.Viewer");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        VIEWER_CLASS = c;
-        try {
-            c = Class.forName("me.filoghost.holographicdisplays.plugin.hologram.tracking.Viewers");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        VIEWERS_CLASS = c;
-        Method m;
-        try {
-            m = LineTracker.class.getDeclaredMethod("sendSpawnPackets", VIEWERS_CLASS);
-        } catch (NoSuchMethodException | SecurityException e) {
-            throw new RuntimeException(e);
-        }
-        LINETRACKER_SEND_SPAWN_METHOD = m;
-        LINETRACKER_SEND_SPAWN_METHOD.setAccessible(true);
-        try {
-            m = LineTracker.class.getDeclaredMethod("sendDestroyPackets", VIEWERS_CLASS);
-        } catch (NoSuchMethodException | SecurityException e) {
-            throw new RuntimeException(e);
-        }
-        LINETRACKER_SEND_DESTROY_METHOD = m;
-        LINETRACKER_SEND_DESTROY_METHOD.setAccessible(true);
-        try {
-            m = MutableViewers.class.getMethod("add", VIEWER_CLASS);
-        } catch (NoSuchMethodException | SecurityException e) {
-            throw new RuntimeException(e);
-        }
-        VIEWERS_ADD_METHOD = m;
-    }
     private final LineTracker<?> delegate;
     private final Map<Player, Object> delegateViewers; // not really Object
     private final Map<Player, Object> removedViewers = new HashMap<>();
 
-    @SuppressWarnings("unchecked")
     public LineTrackerWrapper(LineTracker<?> delegate) {
         this.delegate = delegate;
-        try {
-            delegateViewers = (Map<Player, Object>) VIEWERS_FIELD.get(delegate); // unchecked
-        } catch (IllegalArgumentException | IllegalAccessException e) {
-            throw new RuntimeException(e);
+        delegateViewers = NMSPRovider.getViewers(delegate);
+        // default behaviour to hide from everyone who isn't explicitly shown
+        for (Player player : new ArrayList<>(delegateViewers.keySet())) {
+            stopTracking(player);
         }
     }
 
@@ -88,7 +37,7 @@ public class LineTrackerWrapper {
     public void stopTracking(Player player) {
         Object viewer = delegateViewers.remove(player);
         removedViewers.put(player, viewer);
-        sendPacket(viewer, LINETRACKER_SEND_DESTROY_METHOD);
+        sendPacket(viewer, NMSPRovider.LINETRACKER_SEND_DESTROY_METHOD);
     }
 
     public void startRetracking(Player player) {
@@ -97,7 +46,7 @@ public class LineTrackerWrapper {
             return; // don't write null in case of initial join
         }
         delegateViewers.put(player, viewer);
-        sendPacket(viewer, LINETRACKER_SEND_SPAWN_METHOD);
+        sendPacket(viewer, NMSPRovider.LINETRACKER_SEND_SPAWN_METHOD);
     }
 
     public void onQuit(Player player) {
@@ -112,18 +61,79 @@ public class LineTrackerWrapper {
 
     private void sendPacket(Object viewer, Method sendPacketMethod) {
         MutableViewers<?> viewers = new MutableViewers<>();
-        try {
-            VIEWERS_ADD_METHOD.invoke(viewers, viewer);
-            sendPacketMethod.invoke(delegate, viewers);
-        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
+        NMSPRovider.sendPacket(delegate, viewer, viewers, sendPacketMethod);
     }
 
-    // LineTracker
+    private static class NMSPRovider {
+        private static final Field VIEWERS_FIELD;
+        private static final Class<?> VIEWER_CLASS;
+        private static final Class<?> VIEWERS_CLASS;
+        private static final Method LINETRACKER_SEND_SPAWN_METHOD;
+        private static final Method LINETRACKER_SEND_DESTROY_METHOD;
+        private static final Method VIEWERS_ADD_METHOD;
+        static {
+            Field f;
+            try {
+                f = LineTracker.class.getDeclaredField("viewers");
+            } catch (NoSuchFieldException | SecurityException e) {
+                throw new RuntimeException(e);
+            }
+            VIEWERS_FIELD = f;
+            VIEWERS_FIELD.setAccessible(true);
+            Class<?> c;
+            try {
+                c = Class.forName("me.filoghost.holographicdisplays.plugin.hologram.tracking.Viewer");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            VIEWER_CLASS = c;
+            try {
+                c = Class.forName("me.filoghost.holographicdisplays.plugin.hologram.tracking.Viewers");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            VIEWERS_CLASS = c;
+            Method m;
+            try {
+                m = LineTracker.class.getDeclaredMethod("sendSpawnPackets", VIEWERS_CLASS);
+            } catch (NoSuchMethodException | SecurityException e) {
+                throw new RuntimeException(e);
+            }
+            LINETRACKER_SEND_SPAWN_METHOD = m;
+            LINETRACKER_SEND_SPAWN_METHOD.setAccessible(true);
+            try {
+                m = LineTracker.class.getDeclaredMethod("sendDestroyPackets", VIEWERS_CLASS);
+            } catch (NoSuchMethodException | SecurityException e) {
+                throw new RuntimeException(e);
+            }
+            LINETRACKER_SEND_DESTROY_METHOD = m;
+            LINETRACKER_SEND_DESTROY_METHOD.setAccessible(true);
+            try {
+                m = MutableViewers.class.getMethod("add", VIEWER_CLASS);
+            } catch (NoSuchMethodException | SecurityException e) {
+                throw new RuntimeException(e);
+            }
+            VIEWERS_ADD_METHOD = m;
+        }
 
-    // protected abstract void sendSpawnPackets(Viewers<T> viewers);
+        @SuppressWarnings("unchecked")
+        private static Map<Player, Object> getViewers(LineTracker<?> delegate) {
+            try {
+                return (Map<Player, Object>) VIEWERS_FIELD.get(delegate); // unchecked
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
-    // protected abstract void sendDestroyPackets(Viewers<T> viewers);
+        private static void sendPacket(LineTracker<?> delegate, Object viewer, MutableViewers<?> viewers,
+                Method sendPacketMethod) {
+            try {
+                VIEWERS_ADD_METHOD.invoke(viewers, viewer);
+                sendPacketMethod.invoke(delegate, viewers);
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 }
