039ea7f73b8eae1fb799ade8dff8240887bf9387
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2022-01-14 10:10:24 +0000

    Removed unnecessary test output. Now throwing an exception when trying to mock a server when an implementation already exists

--- src/test/java/me/ford/periodicholographicdisplays/mock/MockStarterUtil.java
+++ src/test/java/me/ford/periodicholographicdisplays/mock/MockStarterUtil.java
@@ -1,58 +1,33 @@
 package me.ford.periodicholographicdisplays.mock;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.lang.reflect.Field;
 
 import org.bukkit.Bukkit;
 
 public final class MockStarterUtil {
-    private static Exception lastStart = null;
-    private static Exception lastFinish = null;
 
     private MockStarterUtil() {
     }
 
     public static MockServer startMocking() {
-        // System.out.println("startMocking:");
         if (Bukkit.getServer() != null) {
-            System.out.println("Server not null when trying to mock...");
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            if (lastStart == null) {
-                System.out.println("Not stopped before");
-            } else {
-                System.out.println("Last Finish:");
-                lastFinish.printStackTrace(pw);
-                System.out.println(sw.toString());
-            }
-            if (lastStart == null) {
-                System.out.println("Not started before???");
-            } else {
-                System.out.println("Last start:");
-                lastStart.printStackTrace(pw);
-                System.out.println(sw.toString());
-            }
-            stopMocking();
+            throw new IllegalStateException(
+                    "Cannot start mocking since a previously mocked server has not been removed. "
+                            + "Call MockStarterUtil#stopMocking first.");
         }
-        lastStart = new IllegalStateException();
         MockServer server;
         Bukkit.setServer(server = new MockServer());
         return server;
     }
 
     public static void stopMocking() {
-        // System.out.println("STOP MOCKING");
         try {
             Field field = Bukkit.class.getDeclaredField("server");
             field.setAccessible(true);
             field.set(null, null);
         } catch (Exception e) {
-            System.out.println("EXCEPTION: " + e);
-            e.printStackTrace();
             throw new RuntimeException(e);
         }
-        lastFinish = new IllegalStateException();
     }
 
     public static MockServer getServer() {
