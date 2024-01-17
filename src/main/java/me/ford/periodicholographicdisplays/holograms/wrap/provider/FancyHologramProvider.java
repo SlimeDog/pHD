b35cd22cbbd6df0c8442b66623902e3cedab7caf
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2024-01-17 09:18:14 +0000

    Fix issue with hologram visibility in FH by listening to show event

--- src/main/java/me/ford/periodicholographicdisplays/holograms/wrap/provider/FancyHologramProvider.java
+++ src/main/java/me/ford/periodicholographicdisplays/holograms/wrap/provider/FancyHologramProvider.java
@@ -7,23 +7,31 @@ import java.util.UUID;
 import java.util.function.Function;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
 import com.google.common.base.Supplier;
 
 import de.oliver.fancyholograms.api.FancyHologramsPlugin;
 import de.oliver.fancyholograms.api.Hologram;
+import de.oliver.fancyholograms.api.events.HologramShowEvent;
+import me.ford.periodicholographicdisplays.PeriodicHolographicDisplays;
+import me.ford.periodicholographicdisplays.holograms.PeriodicType;
 import me.ford.periodicholographicdisplays.holograms.wrap.FancyHologramWrapper;
 import me.ford.periodicholographicdisplays.holograms.wrap.WrappedHologram;
 
 public class FancyHologramProvider implements HologramProvider {
     private final FancyHologramsPlugin fhPlugin;
+    private final PeriodicHolographicDisplays phd;
     private final Function<UUID, Player> playerProvider;
     private final Supplier<Collection<Player>> allPlayersProvider;
 
     public FancyHologramProvider(FancyHologramsPlugin plugin) {
         fhPlugin = plugin;
+        phd = PeriodicHolographicDisplays.getPlugin(PeriodicHolographicDisplays.class);
         playerProvider = (id) -> fhPlugin.getPlugin().getServer().getPlayer(id);
         allPlayersProvider = () -> new ArrayList<>(fhPlugin.getPlugin().getServer().getOnlinePlayers());
+        phd.getServer().getPluginManager().registerEvents(new HologramStatusListener(), phd);
     }
 
     @Override
@@ -39,6 +47,28 @@ public class FancyHologramProvider implements HologramProvider {
             allHolograms.add(new FancyHologramWrapper(playerProvider, allPlayersProvider, holo));
         }
         return allHolograms;
+    }
+
+    private class HologramStatusListener implements Listener {
+
+        @EventHandler
+        public void onHologramShow(HologramShowEvent event) {
+            Hologram hologram = event.getHologram();
+            String hologramName = hologram.getData().getName();
+            List<PeriodicType> availableTypes = phd.getHolograms().getAvailableTypes(hologramName);
+            if (availableTypes.isEmpty()) {
+                return;
+            }
+            Player player = event.getPlayer();
+
+            for (PeriodicType type : availableTypes) {
+                if (phd.getHolograms().getHologram(hologramName, type).isBeingShownTo(player)) {
+                    return;
+                }
+            }
+            // if none should be shown, cancel event
+            event.setCancelled(true);
+        }
     }
 
 }
